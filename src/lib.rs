aa97aa93f814a684c9782f5acffeafdb353d90a5
monochromer <monochromer@mail.ru>
2025-01-22 20:52:56 +0500

    change pointers

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn parse(source: String) -> String {
-    let parser = pulldown_cmark::Parser::new(&source);
+pub fn parse(source: &str) -> String {
+    let parser = pulldown_cmark::Parser::new(source);
     let mut html_output = String::new();
     pulldown_cmark::html::push_html(&mut html_output, parser);
     return html_output;
